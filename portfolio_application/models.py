ec690b999f8c12d6503c26f4bf5385a3c5e68545
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2022-07-12 21:30:25 +0300

    Создал модели навыков и проектов

--- portfolio_application/models.py
+++ portfolio_application/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Skill(models.Model):
+    class Meta():
+        verbose_name = 'Навык'
+        verbose_name_plural = 'Навыки'
+    title = models.CharField(verbose_name='Название навыка', max_length=100)
+    description = models.TextField(verbose_name='Описание навыка')
+    picture = models.ImageField(verbose_name='Картинка навыка')
+
+    def __str__(self):
+        return f'{self.title} {self.description}'
+
+
+class Project(models.Model):
+    class Meta():
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
+    title = models.CharField(verbose_name='Название проекта', max_length=100)
+    description = models.TextField(verbose_name='Описание проекта')
+    picture = models.ImageField(verbose_name='Картинка проекта')
+    link_project = models.URLField(verbose_name='Адрес проекта')
+
+    def __str__(self):
+        return f'{self.title} {self.description} {self.link_project}'
