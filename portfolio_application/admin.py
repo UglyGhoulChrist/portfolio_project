8f9d8c067e601231bbfee3b3f1ebed562c4220f6
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2022-07-12 21:53:05 +0300

    Добавил модели в Админку

--- portfolio_application/admin.py
+++ portfolio_application/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Skill, Project
 
-# Register your models here.
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'picture']
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'picture', 'link_project']
