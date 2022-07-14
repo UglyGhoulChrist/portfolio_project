953e48cb75bf2fec2b9f7a765448dbd1622109ff
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2022-07-14 11:06:02 +0300

    Настроил отображение картинок из моделей через static

--- portfolio_application/models.py
+++ portfolio_application/models.py
@@ -7,7 +7,7 @@ class Skill(models.Model):
         verbose_name_plural = 'Навыки'
     title = models.CharField(verbose_name='Название навыка', max_length=100)
     description = models.TextField(verbose_name='Описание навыка')
-    picture = models.FileField(verbose_name='Картинка навыка', upload_to='media/', null=True, blank=True)
+    picture = models.FileField(verbose_name='Картинка навыка', upload_to='static/portfolio_application/img', null=True, blank=True)
 
     def __str__(self):
         return f'{self.title} {self.description}'
@@ -19,7 +19,7 @@ class Project(models.Model):
         verbose_name_plural = 'Проекты'
     title = models.CharField(verbose_name='Название проекта', max_length=100)
     description = models.TextField(verbose_name='Описание проекта')
-    picture = models.FileField(verbose_name='Картинка проекта', upload_to='media/', null=True, blank=True)
+    picture = models.FileField(verbose_name='Картинка проекта', upload_to='static/portfolio_application/img', null=True, blank=True)
     link_project = models.URLField(verbose_name='Адрес проекта')
 
     def __str__(self):
